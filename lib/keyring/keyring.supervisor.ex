768934564a31e1151cee6ebea42c794f7bdb0443
Jason Clark <mithereal@gmail.com>
2018-01-09 21:46:39 -0700

    add docs

--- lib/keyring/keyring.supervisor.ex
+++ lib/keyring/keyring.supervisor.ex
@@ -11,14 +11,26 @@ def start_link do
 Supervisor.start_link(__MODULE__, nil, name: __MODULE__)
 end
 
-def start(key_id) do
+@doc """
+Start a keyring server.
+## Examples
+    iex> Keyring.Supervisor.start("google")
+    "%{}"
+"""
+def start(id) do
 
-Supervisor.start_child(__MODULE__, [ key_id ])
+Supervisor.start_child(__MODULE__, [ id ])
 end
 
-def stop(key_id) do
+@doc """
+Stop the keyring server.
+## Examples
+    iex> Keyring.Supervisor.stop("google")
+    ":ok"
+"""
+def stop(id) do
 
- case Registry.lookup(@registry_name,  key_id) do
+ case Registry.lookup(@registry_name,  id) do
 
    [] -> :ok
    [{pid, _}] ->
@@ -33,24 +45,42 @@ children = [worker(Keyring, [], restart: :transient)]
 supervise(children, [strategy: :simple_one_for_one])
 end
 
-def find_or_create_process(key_id)  do
+@doc """
+Create a new process if it doesnt exist else return the process.
+## Examples
+    iex> Keyring.Supervisor.find_or_create_process("google")
+    "{}"
+"""
+def find_or_create_process(id)  do
 
- if process_exists?(key_id) do
+ if process_exists?(id) do
 
-   {:ok, key_id}
+   {:ok, id}
  else
-   key_id |> start
+   id |> start
  end
 end
 
-def process_exists?(key_id)  do
+@doc """
+Check if process exists.
+## Examples
+    iex> Keyring.Supervisor.process_exists("google")
+    "true"
+"""
+def process_exists?(id)  do
 
- case Registry.lookup(@registry_name, key_id) do
+ case Registry.lookup(@registry_name, id) do
    [] -> false
    _ -> true
  end
  end
 
+@doc """
+Get the ids of all processes in the registry.
+## Examples
+    iex> Keyring.Supervisor.key_ids("google")
+    "[]"
+"""
 def key_ids do
 
 Supervisor.which_children(__MODULE__)
